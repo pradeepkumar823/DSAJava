b32cf49cb83d9d0c116ab5ed0b0f03dbc884494e
Pradeep Kumar <pradeepkumar.coder08@gmail.com>
2026-07-05 13:44:53 +0530

    [55] [Time Beats: 88.81%] [Memory Beats: 91.34%] - Leet2Hub

--- 01-Arrays-and-Hashing/55. Jump Game/55. Jump Game.java
+++ 01-Arrays-and-Hashing/55. Jump Game/55. Jump Game.java
@@ -1,1 +1,13 @@
-1 <= nums.length <= 104
+1class Solution {
+2    public boolean canJump(int[] nums) {
+3        int n=nums.length;
+4        int goal=0;
+5        for(int i=0;i<n;i++){
+6            if(i > goal){
+7                return false;
+8            }
+9            goal=Math.max(goal,nums[i]+i);
+10        }
+11        return true;
+12    }
+13}
