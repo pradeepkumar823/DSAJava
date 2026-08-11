6345c70d59624e13504e2c9bbd76b3319f0ca4bf
Pradeep Kumar <pradeepkumar.coder08@gmail.com>
2026-08-11 20:58:38 +0530

    [1143] [Time Beats: 91%] [Memory Beats: 91.78%] - Leet2Hub

---
+++ 04-String-Manipulation/1143. Longest Common Subsequence/1143. Longest Common Subsequence.java
@@ -0,0 +1,20 @@
+1class Solution {
+2    public int longestCommonSubsequence(String text1, String text2) {
+3        int n = text1.length();
+4        int m = text2.length();
+5
+6        int[][] dp = new int[n + 1][m + 1];
+7
+8        for (int i = 1; i <= n; i++) {
+9            for (int j = 1; j <= m; j++) {
+10                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+11                    dp[i][j] = dp[i - 1][j - 1] + 1;
+12                } else {
+13                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+14                }
+15            }
+16        }
+17
+18        return dp[n][m];
+19    }
+20}
