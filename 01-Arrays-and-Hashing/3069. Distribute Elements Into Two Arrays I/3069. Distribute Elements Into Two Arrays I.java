ee4d87f728477945fa46be8c5bcfd58302338944
Pradeep Kumar <pradeepkumar.coder08@gmail.com>
2026-08-21 15:57:23 +0530

    [3069] [Time Beats: 7%] [Memory Beats: 7.18%] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/3069. Distribute Elements Into Two Arrays I/3069. Distribute Elements Into Two Arrays I.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] resultArray(int[] nums) {
+        ArrayList<Integer> a = new ArrayList<>();
+        ArrayList<Integer> b = new ArrayList<>();
+
+        a.add(nums[0]);
+        b.add(nums[1]);
+
+        for(int i=2;i<nums.length;i++){
+            if(a.get(a.size()-1)>b.get(b.size()-1)){
+                a.add(nums[i]);
+            }else{
+                b.add(nums[i]);
+            }
+        }
+
+        int[] ans = new int[nums.length];
+        int k = 0;
+
+        for(int x:a){
+            ans[k++] = x;
+        }
+
+        for(int x:b){
+            ans[k++] = x;
+        }
+
+        return ans;
+    }
+}
